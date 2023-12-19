87b9d32a94cf0611a125b1923b12c5be68fa8264
Diêgo Andrade <dandrade@DORTECH.GROUP>
2023-12-19 01:04:36 -0300

    Creation of trait for response

---
+++ app/Traits/HttpResponses.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Traits;
+
+trait HttpResponses {
+    
+    public function response( $message  = 'Success', $data = [],$status = 200){
+        return response()->json([
+            'success'=>true,
+            'message'=>$message,
+            'data'=>$data
+        ],$status);
+    }
+
+    public function error($message, $status = 404){
+        return response()->json([
+            'success' => false,
+            'message' => $message
+        ], $status);
+    }
+}
